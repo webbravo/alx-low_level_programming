2e5472bfb9b1498a49a9ab199efbf6a9313ba0c9
webbravo <4natcom@gmil.com>
2021-06-19 12:46:43 -0700

    Hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: 1.
+ */
+int main(void)
+{
+		fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+						59, 1, stderr);
+			return (1);
+}
