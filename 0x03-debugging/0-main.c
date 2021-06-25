4e838b6cfb60d599ce6e24b4ebb44bbc1c27c073
webbravo <4natcom@gmil.com>
2021-06-25 07:21:41 -0700

    Fixes

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-include "holberton.h"
+#include "holberton.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -7,10 +7,10 @@ include "holberton.h"
 
 int main(void)
 {
-        int i;
+int i;
 
-        i = 0;
-        positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-        return (0);
+return (0);
 }
---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest = (a >= b && a >= c) ? a : (b >= c && b >= a) ? b : c;
+	return (largest);
+}
