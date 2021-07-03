520791cb15e9fa34afad18d189192b7bde2546d4
webbravo <4natcom@gmil.com>
2021-07-03 00:25:49 -0700

    Workn with point in c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-*
-*  reset_to_98 Reset the variable value to 98
-*  Returns: nothing;
-*/
+ * reset_to_98 - takes a pointer to a parameter and updates its value.
+ * @n: input integer.
+ * Return: no return.
+ */
 
 void reset_to_98(int *n)
 {
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: first integer.
+ * @b: second integer.
+ * Return: no return.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
